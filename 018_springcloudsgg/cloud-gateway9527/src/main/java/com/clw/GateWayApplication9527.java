61341f1d99e463a639d76f75f627a6bee4e167be
clw <1348340136@qq.com>
2021-07-05 23:01:23 +0800

    GateWay YML 配置

---
+++ 018_springcloudsgg/cloud-gateway9527/src/main/java/com/clw/GateWayApplication9527.java
@@ -0,0 +1,18 @@
+package com.clw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: clw
+ * @Description:
+ * @Date: 2021/7/5 22:49
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayApplication9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication9527.class, args);
+    }
+}
