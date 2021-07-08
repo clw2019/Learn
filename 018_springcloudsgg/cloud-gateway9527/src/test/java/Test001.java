9f82a848704fc217a60b0fd65b5bb15d7a1ea4bc
clw <1348340136@qq.com>
2021-07-08 22:39:04 +0800

    GateWay 常用Predicate,总共11个

---
+++ 018_springcloudsgg/cloud-gateway9527/src/test/java/Test001.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @Author: clw
+ * @Description:
+ * @Date: 2021/7/8 22:15
+ */
+public class Test001 {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
